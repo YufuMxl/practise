45c8d6b0cae66055769a36b4570c9d01229bdf51
xiaolongma <xiaolongma@yufuid.com>
2022-02-09 22:34:07 +0800

    二分查找的变种

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/BinarySearch.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/BinarySearch.java
@@ -9,8 +9,9 @@ public class BinarySearch {
      * 查找第一个值等于给定值的元素
      *
      * @param a 可包含重复元素的有序数组
+     * @return 要查找的元素的下标
      */
-    public int bsearchFirstValue(int[] a, int value) {
+    public int bsearchFirstEqualValue(int[] a, int value) {
         int lowIndex = 0;
         int highIndex = a.length - 1;
 
@@ -30,4 +31,82 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 查找最后一个值等于给定值的元素
+     *
+     * @param a 可包含重复元素的有序数组
+     * @return 要查找的元素的下标
+     */
+    public int bsearchLastEqualValue(int[] a, int value) {
+        int lowIndex = 0;
+        int highIndex = a.length - 1;
+
+        while (lowIndex <= highIndex) {
+            int midIndex = lowIndex + ((highIndex - lowIndex) >> 1);
+            if (a[midIndex] < value) {
+                lowIndex = midIndex + 1;
+            } else if (a[midIndex] > value) {
+                highIndex = midIndex - 1;
+            } else {
+                if (midIndex == a.length - 1 || a[midIndex + 1] != value) {
+                    return midIndex;
+                } else {
+                    lowIndex = midIndex + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找第一个大于等于给定值的元素
+     *
+     * @param a 可包含重复元素的有序数组
+     * @return 要查找的元素的下标
+     */
+    public int bsearchFirstLargerOrEqualValue(int[] a, int value) {
+        int lowIndex = 0;
+        int highIndex = a.length - 1;
+
+        while (lowIndex <= highIndex) {
+            int midIndex = lowIndex + ((highIndex - lowIndex) >> 1);
+            if (a[midIndex] < value) {
+                lowIndex = midIndex + 1;
+            } else {
+                if (midIndex == 0 || a[midIndex - 1] < value) {
+                    return midIndex;
+                } else {
+                    highIndex = midIndex - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 查找最后一个小于等于给定值的元素
+     *
+     * @param a 可包含重复元素的有序数组
+     * @return 要查找的元素的下标
+     */
+    public int bsearchLastLargerOrEqualValue(int[] a, int value) {
+        int lowIndex = 0;
+        int highIndex = a.length - 1;
+
+        while (lowIndex <= highIndex) {
+            int midIndex = lowIndex + ((highIndex - lowIndex) >> 1);
+            if (a[midIndex] > value) {
+                highIndex = midIndex - 1;
+            } else {
+                if (midIndex == a.length - 1 || a[midIndex + 1] > value) {
+                    return midIndex;
+                } else {
+                    lowIndex = midIndex + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
