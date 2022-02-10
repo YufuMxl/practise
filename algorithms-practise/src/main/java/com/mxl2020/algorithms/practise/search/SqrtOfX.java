76884dafdecca736b001925134ed8e1cea311e19
xiaolongma <xiaolongma@yufuid.com>
2022-02-10 11:21:22 +0800

    用二分查找变体四实现“求满足 k^2<=x 的最大k值”

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/SqrtOfX.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/SqrtOfX.java
@@ -8,38 +8,34 @@ package com.mxl2020.algorithms.practise.search;
 public class SqrtOfX {
 
     /**
-     * 二分查找思路
+     * 二分查找变体四
      * <p>
      * 时间复杂度 O(log n)
      * 空间复杂度 O(1)
      *
-     * @param x 非负整数
      * @return 返回 x 的平方根的整数部分
      */
     public int mySqrt(int x) {
+        // 边界条件
+        if (x < 0) return -1;
+        if (x == 0 || x == 1) return x;
         // 设置区间
-        int low = 0;
-        int high = x;
-        // 初始化中间值
-        int mid = -1;
+        int low = 1;
+        int high = x / 2;
 
         while (low <= high) {
-            // 寻找 low~high 区间的中间数
-            mid = low + ((high - low) >> 1);
-            if ((long) mid * mid == x) {
-                return mid;
-            } else if ((long) mid * mid < x) {
-                low = mid + 1;
-            } else {
+            int mid = low + ((high - low) >> 1);
+            if ((long) mid * mid > x) {
                 high = mid - 1;
+            } else {
+                if (mid == high || (mid + 1) * (mid + 1) > x) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
-
-        if ((long) mid * mid > x) {
-            // 修正 mid 值
-            mid -= 1;
-        }
-        return mid;
+        return -1;
     }
 
     // TODO 如何精确到小数点后 6 位
