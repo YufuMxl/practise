9895f744db5699c199658fc15159e05e81ccded8
xiaolongma <xiaolongma2020@gmail.com>
2022-02-24 21:52:43 +0800

    邻值查找题目描述

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/linkedlist/NeighborLookUp.java
@@ -0,0 +1,26 @@
+package com.mxl2020.algorithms.practise.linkedlist;
+
+/**
+ * <h2>邻值查找
+ * <p>题目条件</p>
+ * <ol>
+ * <li>给定一个长度为 n 的数组 int[] a，a 中的数各不相同
+ * <li>给定下标 i、j，满足 0 <= j < i < n
+ * </ol>
+ * <p>题目要求</p>
+ * <ol>
+ * <li>找到令 a[i] 与 a[j] 差值最小的 j
+ * <li>若满足要求的 a[j] 不唯一，取令 a[j] 最小的那个 j
+ * </ol>
+ * <p>输出</p>
+ * <ol>
+ * <li>从 i=1 开始遍历数组，打印 n-1 行数据，每行数据包括：
+ * <li>打印 abs(a[i] - a[j])
+ * <li>打印 j</p>
+ * </ol>
+ */
+public class NeighborLookUp {
+    public static void main(String[] args) {
+
+    }
+}
