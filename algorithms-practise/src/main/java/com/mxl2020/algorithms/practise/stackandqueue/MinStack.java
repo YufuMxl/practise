5313232189d3b883233dbffd6d581b641798d560
xiaolongma <xiaolongma2020@gmail.com>
2022-02-25 23:30:34 +0800

    最小栈

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/stackandqueue/MinStack.java
@@ -0,0 +1,43 @@
+package com.mxl2020.algorithms.practise.stackandqueue;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 最小栈
+ *
+ * @see <a href="https://leetcode-cn.com/problems/min-stack/">LeetCode 155</a>
+ */
+public class MinStack {
+
+    private final Deque<Integer> mainStack;
+    private final Deque<Integer> minStack;
+
+    public MinStack() {
+        mainStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+}
