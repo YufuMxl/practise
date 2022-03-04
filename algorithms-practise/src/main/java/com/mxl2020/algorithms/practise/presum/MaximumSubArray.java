a4050f331a98e84655838e64d5599c0a24b77493
xiaolongma <xiaolongma2020@gmail.com>
2022-03-04 21:20:13 +0800

    子数组的最大和

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/presum/MaximumSubArray.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/presum/MaximumSubArray.java
@@ -8,6 +8,22 @@ package com.mxl2020.algorithms.practise.presum;
 public class MaximumSubArray {
 
     public int maxSubArray(int[] nums) {
-        return -1;
+
+        // 获取前缀和数组
+        int[] prefixSumArray = new int[nums.length + 1];
+        prefixSumArray[0] = 0;
+        for (int i = 1; i < prefixSumArray.length; i++) {
+            prefixSumArray[i] = prefixSumArray[i - 1] + nums[i - 1];
+        }
+
+        // 求子数组的最大和
+        int minPrefixSum = 0;
+        int maxSumOfSubArray = -10000;
+        for (int i = 1; i < prefixSumArray.length; i++) {
+            minPrefixSum = Math.min(minPrefixSum, prefixSumArray[i - 1]);
+            maxSumOfSubArray = Math.max(maxSumOfSubArray, prefixSumArray[i] - minPrefixSum);
+        }
+
+        return maxSumOfSubArray;
     }
 }
