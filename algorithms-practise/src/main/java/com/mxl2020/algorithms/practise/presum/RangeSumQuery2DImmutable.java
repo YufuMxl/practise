7ea463ed3883d78b6101f129947fb66bfb7a3bd9
xiaolongma <xiaolongma2020@gmail.com>
2022-03-04 22:41:47 +0800

    子矩阵和

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/presum/RangeSumQuery2DImmutable.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/presum/RangeSumQuery2DImmutable.java
@@ -7,11 +7,22 @@ package com.mxl2020.algorithms.practise.presum;
  */
 public class RangeSumQuery2DImmutable {
 
-    public RangeSumQuery2DImmutable(int[][] matrix) {
+    private final int[][] s;
 
+    public RangeSumQuery2DImmutable(int[][] matrix) {
+        s = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 1; i < s.length; i++) {
+            for (int j = 1; j < s[0].length; j++) {
+                s[i][j] = s[i][j - 1] + s[i - 1][j] - s[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return -1;
+        row1++;
+        col1++;
+        row2++;
+        col2++;
+        return s[row2][col2] - s[row2][col1 - 1] - s[row1 - 1][col2] + s[row1 - 1][col1 - 1];
     }
 }
