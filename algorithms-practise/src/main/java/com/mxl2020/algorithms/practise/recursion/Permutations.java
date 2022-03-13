cf0ea7fdd28cb516692d6eeffadb412288e7902a
xiaolongma <xiaolongma2020@gmail.com>
2022-03-13 19:33:57 +0800

    全排列

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/recursion/Permutations.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/recursion/Permutations.java
@@ -1,5 +1,6 @@
 package com.mxl2020.algorithms.practise.recursion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,11 +10,40 @@ import java.util.List;
  */
 public class Permutations {
 
+    private boolean[] used;
+    private final List<Integer> permutation = new ArrayList<>();
+    private final List<List<Integer>> permutations = new ArrayList<>();
+
     /**
      * @param nums 无重复元素的整数数组
      * @return 返回所有的不同排列的数组
      */
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        used = new boolean[nums.length];
+        permute(0, nums);
+        return permutations;
     }
+
+    /**
+     * 往 permutations 数组中插入所有可能的排列数组
+     */
+    private void permute(int depth, int[] nums) {
+        // 终止条件
+        if (depth == nums.length) {
+            permutations.add(new ArrayList<>(permutation));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            permutation.add(nums[i]);
+            permute(depth + 1, nums);
+
+            // 还原共享变量
+            permutation.remove(permutation.size() - 1);
+            used[i] = false;
+        }
+    }
+
 }
