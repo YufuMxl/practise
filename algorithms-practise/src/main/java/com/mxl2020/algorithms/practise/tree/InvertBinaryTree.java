d4c6ad986a5f7bc79d89d22b6f8499664842293a
xiaolongma <xiaolongma2020@gmail.com>
2022-03-15 10:58:06 +0800

    翻转二叉树

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/InvertBinaryTree.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/InvertBinaryTree.java
@@ -8,7 +8,17 @@ import com.mxl2020.algorithms.practise.tree.datastructure.TreeNode;
  * @see <a href="https://leetcode-cn.com/problems/invert-binary-tree/">LeetCode 226</a>
  */
 public class InvertBinaryTree {
+
     public TreeNode invertTree(TreeNode root) {
-        return null;
+        // 递归终止条件：如果 root 为空，或者 root 是叶子节点，则不需要翻转，直接返回即可
+        if (root == null || (root.left == null && root.right == null)) return root;
+
+        // 翻转
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.right = invertTree(left);
+        root.left = invertTree(right);
+
+        return root;
     }
 }
