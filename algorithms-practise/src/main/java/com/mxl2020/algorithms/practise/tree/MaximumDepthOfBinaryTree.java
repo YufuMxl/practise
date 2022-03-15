7e9bce4c41834fe2db40725ff7e8d4a03158a63e
xiaolongma <xiaolongma2020@gmail.com>
2022-03-15 16:39:13 +0800

    二叉树最大深度的代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/MaximumDepthOfBinaryTree.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/MaximumDepthOfBinaryTree.java
@@ -13,4 +13,30 @@ public class MaximumDepthOfBinaryTree {
         if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
+
+    /**
+     * 解法 2：递归 + 全局变量
+     *
+     * @param root 树的节点
+     * @return 返回 root 所代表的树的深度
+     */
+    public int maxDepth2(TreeNode root) {
+        calculateDepth(root);
+        return result;
+    }
+
+    private int depth = 0;
+    private int result = 0;
+
+    private void calculateDepth(TreeNode root) {
+        if (root == null) {
+            // 递归出界，更新答案
+            result = Math.max(depth, result);
+            return;
+        }
+        depth++;
+        calculateDepth(root.left);
+        calculateDepth(root.right);
+        depth--;
+    }
 }
