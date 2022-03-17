b72aa65e6acb5ff5094d03f74e1f911599e3dbfc
xiaolongma <xiaolongma2020@gmail.com>
2022-03-17 11:42:51 +0800

    括号生成代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/recursion/GenerateParentheses.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/recursion/GenerateParentheses.java
@@ -11,6 +11,9 @@ import java.util.List;
  */
 public class GenerateParentheses {
 
+    // 以 n 为下标，记录"生成括号"的结果
+    private List<String>[] store;
+
     /**
      * 分治解法
      * <p>
@@ -19,24 +22,34 @@ public class GenerateParentheses {
      * 该题中，划分子问题的正确方法：(A)B
      *
      * @param n n 对括号
-     * @return 返回括号的所有合法排列
+     * @return 返回所有有效的括号组合
      */
     public List<String> generateParenthesis(int n) {
-        if (n == 0) return Collections.singletonList("");
-        List<String> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            // 子问题 A
-            List<String> aParentheses = generateParenthesis(i);
-            // 子问题 B
-            List<String> bParentheses = generateParenthesis(n - i - 1);
-            // 合并子问题
-            for (String aParenthesis : aParentheses) {
-                for (String bParenthesis : bParentheses) {
-                    ans.add("(" + aParenthesis + ")" + bParenthesis);
+        // 初始化 store
+        if (store == null) store = new List[n + 1];
+        // 先从 store 里面获取结果
+        if (store[n] != null) return store[n];
+
+        List<String> ans;
+        if (n == 0) {
+            // 递归终止条件
+            ans = Collections.singletonList("");
+        } else {
+            ans = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                // 子问题 A
+                List<String> aParentheses = generateParenthesis(i);
+                // 子问题 B
+                List<String> bParentheses = generateParenthesis(n - i - 1);
+                // 合并子问题
+                for (String aParenthesis : aParentheses) {
+                    for (String bParenthesis : bParentheses) {
+                        ans.add("(" + aParenthesis + ")" + bParenthesis);
+                    }
                 }
             }
         }
-
+        store[n] = ans;
         return ans;
     }
 }
