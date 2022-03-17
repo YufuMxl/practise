a2d8c1f6ed103529868b784fe7781680f500e1d2
xiaolongma <xiaolongma2020@gmail.com>
2022-03-17 16:14:18 +0800

    n 叉树的前序遍历实现

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/N_AryTreePreorderTraversal.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/N_AryTreePreorderTraversal.java
@@ -2,6 +2,7 @@ package com.mxl2020.algorithms.practise.tree;
 
 import com.mxl2020.algorithms.practise.tree.datastructure.Node;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,7 +11,23 @@ import java.util.List;
  * @see <a href="https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/">LeetCode 589</a>
  */
 public class N_AryTreePreorderTraversal {
+
+    private List<Integer> sequence;
+
     public List<Integer> preorder(Node root) {
-        return null;
+        sequence = new ArrayList<>();
+        dfs(root);
+        return sequence;
+    }
+
+    private void dfs(Node node) {
+        // 递归终止条件
+        if (node == null) return;
+        sequence.add(node.val);
+        if (node.children != null) {
+            for (Node child : node.children) {
+                dfs(child);
+            }
+        }
     }
 }
