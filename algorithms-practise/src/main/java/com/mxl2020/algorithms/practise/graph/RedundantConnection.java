11cfde80487d80c002540c488ec1a065c51d4e83
xiaolongma <xiaolongma2020@gmail.com>
2022-03-20 14:34:50 +0800

    冗余连接的实现

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/RedundantConnection.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/RedundantConnection.java
@@ -1,5 +1,9 @@
 package com.mxl2020.algorithms.practise.graph;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 冗余连接
  *
@@ -7,7 +11,7 @@ package com.mxl2020.algorithms.practise.graph;
  */
 public class RedundantConnection {
     /**
-     * 深度优先遍历找环，环上删除一边
+     * 深度优先遍历
      * <p>
      * 树的定义：n 个节点，n - 1 条边的"无环、无向、连通图"
      * <p>
@@ -17,7 +21,36 @@ public class RedundantConnection {
      * @return 找到形成基环树的那条边并返回，如果答案有多个，返回 edges 中最后出现的那条边
      */
     public int[] findRedundantConnection(int[][] edges) {
+        // 初始化"遍历记录表"
+        visited = new boolean[edges.length + 1];
+        // 初始化出边数组
+        graph = new ArrayList<>(edges.length + 1);
+        for (int i = 0; i <= edges.length; i++) {
+            graph.add(i, new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+            dfs(edge[0], 0);
+            if (hasCycle) return edge;
+            else Arrays.fill(visited, false);
+        }
         return null;
+    }
+
+    private List<List<Integer>> graph;
+    private boolean[] visited;
+    private boolean hasCycle;
+
+    private void dfs(int x, int father) {
+        visited[x] = true;
+        for (int y : graph.get(x)) {
+            // 递归终止条件：遇到父节点
+            if (y == father) continue;
+            // 递归终止条件：此处成环
+            if (visited[y]) hasCycle = true;
+            else dfs(y, x);
+        }
     }
 
 }
