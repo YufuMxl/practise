b14ca3a8ed4a87d67ef36a8fb5e868b59ff6d0ae
xiaolongma <xiaolongma2020@gmail.com>
2022-03-20 12:19:54 +0800

    冗余连接注释更新

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/RedundantConnection.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/RedundantConnection.java
@@ -6,9 +6,18 @@ package com.mxl2020.algorithms.practise.graph;
  * @see <a href="https://leetcode-cn.com/problems/redundant-connection/">LeetCode 684</a>
  */
 public class RedundantConnection {
+    /**
+     * 深度优先遍历找环，环上删除一边
+     * <p>
+     * 树的定义：n 个节点，n - 1 条边的"无环、无向、连通图"
+     * <p>
+     * 基环树的定义：给树多加一条边
+     *
+     * @param edges 基环树的边，边数为 n
+     * @return 找到形成基环树的那条边并返回，如果答案有多个，返回 edges 中最后出现的那条边
+     */
     public int[] findRedundantConnection(int[][] edges) {
         return null;
     }
 
-    // 数据实际上是一棵基环树。深度优先遍历找环，环上删除一边
 }
