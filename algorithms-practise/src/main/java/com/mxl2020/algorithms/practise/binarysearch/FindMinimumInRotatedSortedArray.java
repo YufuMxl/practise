eb19f9bd0afdf721e6422d2ba4129c52668a2d58
xiaolongma <xiaolongma2020@gmail.com>
2022-03-24 16:40:19 +0800

    寻找旋转排序数组中的最小值

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/binarysearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+package com.mxl2020.algorithms.practise.binarysearch;
+
+/**
+ * 寻找旋转排序数组中的最小值
+ *
+ * @see <a href="https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/">LeetCode 153</a>
+ */
+public class FindMinimumInRotatedSortedArray {
+
+    /**
+     * @param nums 无重复元素的"旋转排序"的整数数组
+     * @return 返回 nums 中的最小值
+     */
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int lastNum = nums[right];
+
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] > lastNum) {
+                left = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] > nums[mid]) {
+                    return nums[mid];
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
