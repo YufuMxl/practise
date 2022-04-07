e69712f76d0a470ef3cf7479650ee57ea0214909
xiaolongma <xiaolongma2020@gmail.com>
2022-04-07 18:50:06 +0800

    四数之和

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/hashtable/FourSum.java
@@ -0,0 +1,20 @@
+package com.mxl2020.algorithms.practise.hashtable;
+
+import java.util.List;
+
+/**
+ * 四数之和
+ *
+ * @see <a href="https://leetcode-cn.com/problems/4sum/">LeetCode 18</a>
+ */
+public class FourSum {
+
+    /**
+     * @param nums   可重复元素的整数数组
+     * @param target 目标和
+     * @return 返回元素之和等于 target 的所有子数组集合，任意子数组之间的内容不能重复
+     */
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
