8b4671493b08db1ffe737cab61817c8358fe0985
xiaolongma <xiaolongma2020@gmail.com>
2022-04-20 18:21:13 +0800

    买卖股票代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/greedy/BestTimeBuySellStockII.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/greedy/BestTimeBuySellStockII.java
@@ -45,4 +45,12 @@ public class BestTimeBuySellStockII {
         }
         return profit;
     }
+
+    public int maxProfit2(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            profit += Math.max(prices[i] - prices[i - 1], 0);
+        }
+        return profit;
+    }
 }
