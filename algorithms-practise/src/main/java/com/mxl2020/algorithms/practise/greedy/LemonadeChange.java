83e8aecfe5225074f9a68f9231db5447590dcbf6
xiaolongma <xiaolongma2020@gmail.com>
2022-04-20 09:54:24 +0800

    柠檬水找零

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/greedy/LemonadeChange.java
@@ -0,0 +1,12 @@
+package com.mxl2020.algorithms.practise.greedy;
+
+/**
+ * 柠檬水找零
+ *
+ * @see <a href="https://leetcode-cn.com/problems/lemonade-change/">LeetCode 860</a>
+ */
+public class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        return false;
+    }
+}
