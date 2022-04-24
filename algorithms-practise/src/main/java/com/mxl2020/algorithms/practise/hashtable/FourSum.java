0afe0b62b3628c18108321f74a69637f84d64257
xiaolongma <xiaolongma2020@gmail.com>
2022-04-24 18:55:28 +0800

    四数之和

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/hashtable/FourSum.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/hashtable/FourSum.java
@@ -1,5 +1,7 @@
 package com.mxl2020.algorithms.practise.hashtable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,6 +17,40 @@ public class FourSum {
      * @return 返回元素之和等于 target 的所有子数组集合，任意子数组之间的内容不能重复
      */
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        final int lastIndex = nums.length - 1;
+
+        for (int a = 0; a < lastIndex - 2; a++) {
+            if (a > 0 && nums[a] == nums[a - 1]) continue;
+
+            for (int b = a + 1; b < lastIndex - 1; b++) {
+                if (b > a + 1 && nums[b] == nums[b - 1]) continue;
+
+                int c = b + 1;
+                int d = lastIndex;
+
+                int abSum = nums[a] + nums[b];
+
+                while (c < d) {
+                    int sum = abSum + nums[c] + nums[d];
+                    if (sum < target) c++;
+                    else if (sum > target) d--;
+                    else {
+                        ans.add(Arrays.asList(nums[a], nums[b], nums[c], nums[d]));
+                        while (c < d && nums[c] == nums[c + 1]) {
+                            c++;
+                        }
+                        while (c < d && nums[d] == nums[d - 1]) {
+                            d--;
+                        }
+                        c++;
+                        d--;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
