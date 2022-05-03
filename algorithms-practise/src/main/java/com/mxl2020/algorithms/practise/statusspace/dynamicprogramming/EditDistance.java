83115be443c983ba03f9703a3cbba75ed964163c
xiaolongma <xiaolongma2020@gmail.com>
2022-05-03 19:56:39 +0800

    编辑距离

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/statusspace/dynamicprogramming/EditDistance.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/statusspace/dynamicprogramming/EditDistance.java
@@ -13,16 +13,16 @@ public class EditDistance {
      * @return 返回将 1 变成 2 的最小编辑次数（编辑方式有插入、删除、替换一个字符）
      */
     public int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        int[][] opt = new int[word1.length() + 1][word2.length() + 1];
 
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                if (i == 0 || j == 0) dp[i][j] = i + j;
-                else if (word1.charAt(i - 1) == word2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1));
+        for (int i = 0; i < opt.length; i++) {
+            for (int j = 0; j < opt[0].length; j++) {
+                if (i == 0 || j == 0) opt[i][j] = i + j;
+                else if (word1.charAt(i - 1) == word2.charAt(j - 1)) opt[i][j] = opt[i - 1][j - 1];
+                else opt[i][j] = Math.min(opt[i - 1][j - 1] + 1, Math.min(opt[i - 1][j] + 1, opt[i][j - 1] + 1));
             }
         }
 
-        return dp[dp.length - 1][dp[0].length - 1];
+        return opt[word1.length()][word2.length()];
     }
 }
