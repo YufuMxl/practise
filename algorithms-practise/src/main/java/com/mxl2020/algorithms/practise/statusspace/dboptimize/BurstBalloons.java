46cbec110f468d89f75f3f28fd259da77cb204c3
xiaolongma <xiaolongma2020@gmail.com>
2022-05-15 11:43:25 +0800

    戳气球

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/statusspace/dboptimize/BurstBalloons.java
@@ -0,0 +1,18 @@
+package com.mxl2020.algorithms.practise.statusspace.dboptimize;
+
+/**
+ * 戳气球
+ *
+ * @see <a href="https://leetcode.cn/problems/burst-balloons/">LeetCode 312</a>
+ */
+public class BurstBalloons {
+
+
+    /**
+     * @param nums 下标为气球编号，元素值为气球上的数字
+     * @return 计算戳破所有的气球，能获得硬币的最大数量
+     */
+    public int maxCoins(int[] nums) {
+        return -1;
+    }
+}
