089d751f71c62c94994489fb98037a0d4ad059e7
xiaolongma <xiaolongma2020@gmail.com>
2022-06-06 09:59:03 +0800

    单词搜索 II：代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/trie/WordSearchII.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/trie/WordSearchII.java
@@ -8,108 +8,74 @@ import java.util.*;
  * @see <a href="https://leetcode.cn/problems/word-search-ii/">LeetCode 212</a>
  */
 public class WordSearchII {
-    private Set<String> ans;
+    private List<String> ans;
     private char[][] board;
     private int m;
     private int n;
     private boolean[][] visited;
     private StringBuilder wordBuilder;
+    private TrieNode root;
 
     private final int[][] directions = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
 
     public List<String> findWords(char[][] board, String[] words) {
-        this.ans = new HashSet<>();
+        this.ans = new ArrayList<>();
         this.board = board;
         this.m = board.length;
         this.n = board[0].length;
         this.visited = new boolean[m][n];
         this.wordBuilder = new StringBuilder();
 
-        // 生生 Trie 树
-        Trie trie = new Trie();
+        // 生成 Trie 树
+        this.root = new TrieNode();
         for (String word : words) {
-            trie.insert(word);
+            insert(word);
         }
 
         // 深度优先遍历 board
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                dfs(i, j, trie.root);
+                dfs(i, j, this.root);
             }
         }
 
-        return new ArrayList<>(ans);
+        return ans;
     }
 
     private void dfs(final int x, final int y, final TrieNode node) {
-        TrieNode currentNode = node.children.get(board[x][y]);
+        TrieNode currentNode = node.children[board[x][y] - 'a'];
         if (currentNode == null) return;
 
         wordBuilder.append(board[x][y]);
-        if (currentNode.count > 0) {
-            currentNode.count = 0;
+        if (currentNode.wordTag) {
             ans.add(wordBuilder.toString());
+            currentNode.wordTag = false;
         }
         visited[x][y] = true;
         for (int[] direction : directions) {
             int subX = x + direction[0];
             int subY = y + direction[1];
-            if (subX < 0 || subX == m || subY < 0 || subY == n) continue;
-            if (visited[subX][subY]) continue;
+            if (subX < 0 || subX == m || subY < 0 || subY == n || visited[subX][subY]) continue;
             dfs(subX, subY, currentNode);
         }
         visited[x][y] = false;
         wordBuilder.deleteCharAt(wordBuilder.length() - 1);
     }
 
-    static class Trie {
-        private final TrieNode root;
-
-        public Trie() {
-            this.root = new TrieNode();
-        }
-
-        public void insert(String word) {
-            TrieNode node = root;
-            for (char c : word.toCharArray()) {
-                if (node.children.get(c) == null) {
-                    node.children.put(c, new TrieNode());
-                }
-                node = node.children.get(c);
+    private void insert(String word) {
+        TrieNode node = this.root;
+        for (char c : word.toCharArray()) {
+            int cIndex = c - 'a';
+            if (node.children[cIndex] == null) {
+                node.children[cIndex] = new TrieNode();
             }
-            node.count += 1;
+            node = node.children[cIndex];
         }
-
-        public boolean search(String word) {
-            TrieNode node = root;
-            for (char c : word.toCharArray()) {
-                if (node.children.get(c) == null) {
-                    return false;
-                }
-                node = node.children.get(c);
-            }
-            return node.count > 0;
-        }
-
-        public boolean startsWith(String prefix) {
-            TrieNode node = root;
-            for (char c : prefix.toCharArray()) {
-                if (node.children.get(c) == null) {
-                    return false;
-                }
-                node = node.children.get(c);
-            }
-            return true;
-        }
+        node.wordTag = true;
     }
 
     static class TrieNode {
-        public int count;
-        public final Map<Character, TrieNode> children;
-
-        public TrieNode() {
-            this.count = 0;
-            this.children = new HashMap<>();
-        }
+        public boolean wordTag;
+        public final TrieNode[] children = new TrieNode[26];
     }
 }
