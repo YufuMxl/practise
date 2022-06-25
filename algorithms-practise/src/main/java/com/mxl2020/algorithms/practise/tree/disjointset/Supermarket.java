9eb6e8ea9269d0bf459782647ef44a447496915e
xiaolongma <xiaolongma2020@gmail.com>
2022-06-25 19:30:15 +0800

    超市

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/disjointset/Supermarket.java
@@ -0,0 +1,21 @@
+package com.mxl2020.algorithms.practise.tree.disjointset;
+
+import javafx.util.Pair;
+
+/**
+ * 超市
+ *
+ * @see <a href="https://www.acwing.com/problem/content/147/">AcWing 145</a>
+ */
+public class Supermarket {
+
+    /**
+     * 贪心 + 路径压缩
+     * @param profitDayPair profit[i] 表示商品 i 的利润
+     *                      date[i] 表示商品 i 的过期时间
+     * @return 总共 N 个商品，但每天只能卖一个商品，过期商品不能再卖，求最大利润
+     */
+    public int solve(Pair<Integer, Integer>[] profitDayPair) {
+        return -1;
+    }
+}
