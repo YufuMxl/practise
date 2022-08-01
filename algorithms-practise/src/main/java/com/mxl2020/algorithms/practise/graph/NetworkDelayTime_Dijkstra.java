9bc104489e5f1f92d840be5e5988040212bedfd4
xiaolongma <xiaolongma2020@gmail.com>
2022-08-01 17:10:18 +0800

    网络延迟时间

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/NetworkDelayTime_Dijkstra.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/graph/NetworkDelayTime_Dijkstra.java
@@ -1,8 +1,6 @@
 package com.mxl2020.algorithms.practise.graph;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class NetworkDelayTime_Dijkstra {
 
@@ -47,5 +45,45 @@ public class NetworkDelayTime_Dijkstra {
         }
         expand[minDistVertex] = true;
         return minDistVertex;
+    }
+
+    public int networkDelayTime2(int[][] times, int n, int k) {
+        // 初始化带权出边数组
+        List<ArrayList<int[]>> adjacencyArray = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            adjacencyArray.add(i, new ArrayList<>());
+        }
+        for (int[] time : times) {
+            adjacencyArray.get(time[0]).add(new int[]{time[1], time[2]});
+        }
+
+        // 初始化 dist 数组
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, (int) 1e9);
+        dist[k] = 0;
+        // 初始化 expand 数组
+        boolean[] expand = new boolean[n + 1];
+        // 初始化优先队列
+        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        q.offer(new int[]{k, 0});
+
+        while (!q.isEmpty()) {
+            int[] top = q.poll();
+            int minDistVertex = top[0];
+            if (expand[minDistVertex]) continue;
+            expand[minDistVertex] = true;
+            for (int[] edge : adjacencyArray.get(minDistVertex)) {
+                if (dist[edge[0]] > dist[minDistVertex] + edge[1]) {
+                    dist[edge[0]] = dist[minDistVertex] + edge[1];
+                    q.offer(new int[]{edge[0], dist[edge[0]]});
+                }
+            }
+        }
+
+        int delayTime = dist[1];
+        for (int i = 2; i <= n; i++) {
+            delayTime = Math.max(delayTime, dist[i]);
+        }
+        return delayTime == (int) 1e9 ? -1 : delayTime;
     }
 }
