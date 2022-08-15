c09ad25f763bd210cbdf9486f61ab7e88c98e16f
xiaolongma <xiaolongma2020@gmail.com>
2022-08-15 11:04:52 +0800

    被围绕的区域 dfs

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/disjointset/SurroundedRegions.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/disjointset/SurroundedRegions.java
@@ -19,6 +19,11 @@ public class SurroundedRegions {
         }
     }
 
+    /**
+     * 解法 1：并查集
+     *
+     * @param board m * n 的矩阵
+     */
     public void solve(char[][] board) {
         final int m = board.length;
         final int n = board[0].length;
@@ -63,6 +68,52 @@ public class SurroundedRegions {
         b = find(b);
         if (a != b) fa[a] = b;
     }
+
+    /**
+     * 解法 2：dfs
+     *
+     * @param board m * n 的矩阵
+     */
+    public void solve2(char[][] board) {
+        final int m = board.length;
+        final int n = board[0].length;
+        this.board = board;
+        this.visited = new boolean[m][n];
+
+        final int m_1 = m - 1;
+        final int n_1 = n - 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == m_1 || j == n_1) {
+                    if (board[i][j] == 'O' && !visited[i][j]) {
+                        visited[i][j] = true;
+                        dfs(i, j);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]) board[i][j] = 'X';
+            }
+        }
+    }
+
+    private char[][] board;
+    private boolean[][] visited;
+    private final int[][] fourDirections = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+
+    private void dfs(final int x, final int y) {
+        for (int[] direction : fourDirections) {
+            int subX = x + direction[0];
+            int subY = y + direction[1];
+            if (subX < 0 || subX >= board.length || subY < 0 || subY >= board[0].length) continue;
+            if (board[subX][subY] == 'X') continue;
+            if (visited[subX][subY]) continue;
+            visited[subX][subY] = true;
+            dfs(subX, subY);
+        }
+    }
 }
 
-// TODO dfs
