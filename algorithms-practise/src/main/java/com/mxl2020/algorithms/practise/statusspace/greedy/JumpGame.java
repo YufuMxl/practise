1ab65ab36335601284760c9225dd1fafaea8f53c
longxiaoma <xiaolongma2020@gmail.com>
2022-09-16 18:14:33 +0800

    跳跃游戏

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/statusspace/greedy/JumpGame.java
@@ -0,0 +1,17 @@
+package com.mxl2020.algorithms.practise.statusspace.greedy;
+
+/**
+ * 跳跃游戏
+ *
+ * @see <a href="https://leetcode.cn/problems/jump-game/">LeetCode 55</a>
+ */
+public class JumpGame {
+
+    /**
+     * @param nums 非负整数数组
+     * @return 判断是否能够到达 nums 的最后一个下标
+     */
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+}
