f442b0951e4c8ce75ca3662a3c60d4e8b01e2fa1
longxiaoma <xiaolongma2020@gmail.com>
2022-10-24 18:45:18 +0800

    合并两个有序链表代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/linkedlist/MergeTwoSortedLists.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/linkedlist/MergeTwoSortedLists.java
@@ -17,12 +17,8 @@ public class MergeTwoSortedLists {
      */
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         // 边界条件 + 终止条件
-        if (list1 == null) {
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
 
         if (list1.val < list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
