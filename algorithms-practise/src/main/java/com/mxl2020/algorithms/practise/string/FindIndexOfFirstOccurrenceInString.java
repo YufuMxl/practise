fa1dbee8ce8adf6efd512fee6ca57045dca62521
xiaolongma <3411971052@qq.com>
2022-11-11 09:02:14 +0800

    找出字符串中第一个匹配项的下标

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/string/FindIndexOfFirstOccurrenceInString.java
@@ -0,0 +1,13 @@
+package com.mxl2020.algorithms.practise.string;
+
+/**
+ * 找出字符串中第一个匹配项的下标
+ *
+ * @see <a href="https://leetcode.cn/problems/find-the-index-of-the-first-occurrence-in-a-string/">LeetCode 28</a>
+ */
+public class FindIndexOfFirstOccurrenceInString {
+
+    public int strStr(String haystack, String needle) {
+
+    }
+}
