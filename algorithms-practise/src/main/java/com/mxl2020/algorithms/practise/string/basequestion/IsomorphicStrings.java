98ebbb0eb7f516ba5f45058e69ff9391782eb654
longxiaoma <longxiaoma@tencent.com>
2022-11-14 19:16:48 +0800

    同构字符串

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/string/basequestion/IsomorphicStrings.java
@@ -0,0 +1,27 @@
+package com.mxl2020.algorithms.practise.string.basequestion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 同构字符串
+ *
+ * @see <a href="https://leetcode.cn/problems/isomorphic-strings/">LeetCode 205</a>
+ */
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> s2t = new HashMap<>();
+        Map<Character, Character> t2s = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char x = s.charAt(i);
+            char y = t.charAt(i);
+
+            if ((s2t.containsKey(x) && s2t.get(x) != y) || t2s.containsKey(y) && t2s.get(y) != x) return false;
+
+            s2t.put(x, y);
+            t2s.put(y, x);
+        }
+        return true;
+    }
+}
