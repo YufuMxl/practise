3b2d7486a79f27387e0d17fab0cd02659f665064
xiaolongma <3411971052@qq.com>
2022-11-27 22:59:05 +0800

    通配符匹配

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/string/WildcardMatching.java
@@ -0,0 +1,13 @@
+package com.mxl2020.algorithms.practise.string;
+
+/**
+ * 通配符匹配
+ *
+ * @see <a href="https://leetcode.cn/problems/wildcard-matching/">LeetCode 44</a>
+ */
+public class WildcardMatching {
+
+    public boolean isMatch(String s, String p) {
+        return false;
+    }
+}
