7283cedeff986eb3611f6c41ac551178906f3db1
xiaolongma <3411971052@qq.com>
2022-12-01 11:01:00 +0800

    通配符匹配

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/string/WildcardMatching.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/string/WildcardMatching.java
@@ -8,6 +8,29 @@ package com.mxl2020.algorithms.practise.string;
 public class WildcardMatching {
 
     public boolean isMatch(String s, String p) {
-        return false;
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') f[0][j] = true;
+            else break;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char pChar = p.charAt(j - 1);
+                if (pChar == '*') {
+                    f[i][j] = f[i - 1][j - 1] || f[i][j - 1] || f[i - 1][j];
+                } else if (pChar == '?') {
+                    f[i][j] = f[i - 1][j - 1];
+                } else {
+                    if (s.charAt(i - 1) == pChar) f[i][j] = f[i - 1][j - 1];
+                }
+            }
+        }
+
+        return f[m][n];
     }
 }
