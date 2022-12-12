18753888ed4114c7909b3edb0a1e97f9b19c665f
xiaolongma <3411971052@qq.com>
2022-12-12 16:48:49 +0800

    数独

---
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
@@ -0,0 +1,14 @@
+package com.mxl2020.algorithms.practise.simulation;
+
+/**
+ * 有效的数独 +  解数独
+ *
+ * @see <a href="https://leetcode.cn/problems/valid-sudoku/">LeetCode 36</a><a href="https://leetcode.cn/problems/sudoku-solver/">LeetCode 37</a>
+ */
+public class Sudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        return false;
+    }
+
+}
