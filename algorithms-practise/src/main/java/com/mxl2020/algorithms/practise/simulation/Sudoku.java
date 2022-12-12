7399fd58e3ec0bdd1621d97a3086b5eaf3810448
xiaolongma <3411971052@qq.com>
2022-12-12 20:26:27 +0800

    有效的数独

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
@@ -8,7 +8,28 @@ package com.mxl2020.algorithms.practise.simulation;
 public class Sudoku {
 
     public boolean isValidSudoku(char[][] board) {
-        return false;
+        char[][] row = new char[9][9];
+        char[][] col = new char[9][9];
+        char[][] box = new char[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c == '.') continue;
+
+                int cIndex = c - '1';
+                if (row[i][cIndex] != 0) return false;
+                else row[i][cIndex] = c;
+
+                if (col[j][cIndex] != 0) return false;
+                else col[j][cIndex] = c;
+
+                int boxNum = i / 3 * 3 + j / 3;
+                if (box[boxNum][cIndex] != 0) return false;
+                else box[boxNum][cIndex] = c;
+            }
+        }
+        return true;
     }
 
 }
