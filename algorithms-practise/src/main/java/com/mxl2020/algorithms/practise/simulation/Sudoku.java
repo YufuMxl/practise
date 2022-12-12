452b7b3368a165bf70588bbb02da51e7d6be0a9c
xiaolongma <3411971052@qq.com>
2022-12-12 22:13:26 +0800

    有效的数独 代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
@@ -1,16 +1,16 @@
 package com.mxl2020.algorithms.practise.simulation;
 
 /**
- * 有效的数独 +  解数独
+ * 有效的数独 + 解数独
  *
  * @see <a href="https://leetcode.cn/problems/valid-sudoku/">LeetCode 36</a><a href="https://leetcode.cn/problems/sudoku-solver/">LeetCode 37</a>
  */
 public class Sudoku {
 
     public boolean isValidSudoku(char[][] board) {
-        char[][] row = new char[9][9];
-        char[][] col = new char[9][9];
-        char[][] box = new char[9][9];
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
@@ -18,15 +18,15 @@ public class Sudoku {
                 if (c == '.') continue;
 
                 int cIndex = c - '1';
-                if (row[i][cIndex] != 0) return false;
-                else row[i][cIndex] = c;
+                if (row[i][cIndex]) return false;
+                else row[i][cIndex] = true;
 
-                if (col[j][cIndex] != 0) return false;
-                else col[j][cIndex] = c;
+                if (col[j][cIndex]) return false;
+                else col[j][cIndex] = true;
 
                 int boxNum = i / 3 * 3 + j / 3;
-                if (box[boxNum][cIndex] != 0) return false;
-                else box[boxNum][cIndex] = c;
+                if (box[boxNum][cIndex]) return false;
+                else box[boxNum][cIndex] = true;
             }
         }
         return true;
