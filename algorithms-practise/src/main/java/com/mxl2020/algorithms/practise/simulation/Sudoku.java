c3a31f780e8d81295bf131d817a0bbd4ffaacda2
xiaolongma <3411971052@qq.com>
2022-12-13 17:07:32 +0800

    解数独

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/simulation/Sudoku.java
@@ -1,5 +1,8 @@
 package com.mxl2020.algorithms.practise.simulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 有效的数独 + 解数独
  *
@@ -30,6 +33,66 @@ public class Sudoku {
             }
         }
         return true;
+    }
+
+
+    boolean[][] rowUsed = new boolean[9][10];
+    boolean[][] colUsed = new boolean[9][10];
+    boolean[][] boxUsed = new boolean[9][10];
+
+    private char[][] board;
+    private final List<int[]> emptyCells = new ArrayList<>();
+
+    public void solveSudoku(char[][] board) {
+        this.board = board;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    emptyCells.add(new int[]{i, j});
+                    continue;
+                }
+
+                int num = board[i][j] - '0';
+                rowUsed[i][num] = true;
+                colUsed[j][num] = true;
+                boxUsed[(i / 3) * 3 + (j / 3)][num] = true;
+            }
+        }
+        dfs(0);
+    }
+
+    private boolean dfs(int index) {
+        if (index == emptyCells.size()) return true;
+
+        int x = emptyCells.get(index)[0];
+        int y = emptyCells.get(index)[1];
+
+        List<Integer> validNumbers = find(x, y);
+        for (int num : validNumbers) {
+            int boxNum = (x / 3) * 3 + (y / 3);
+            board[x][y] = (char) (num + '0');
+            rowUsed[x][num] = true;
+            colUsed[y][num] = true;
+            boxUsed[boxNum][num] = true;
+
+            if (dfs(index + 1)) return true;
+
+            rowUsed[x][num] = false;
+            colUsed[y][num] = false;
+            boxUsed[boxNum][num] = false;
+        }
+
+        board[x][y] = '.';
+        return false;
+    }
+
+    private List<Integer> find(int x, int y) {
+        int boxNum = (x / 3) * 3 + (y / 3);
+        List<Integer> validNumbers = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            if (!rowUsed[x][i] && !colUsed[y][i] && !boxUsed[boxNum][i]) validNumbers.add(i);
+        }
+        return validNumbers;
     }
 
 }
