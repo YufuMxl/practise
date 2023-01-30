873476808c0ce4a113ced850b140fdd1e88b8aed
xiaolongma <3411971052@qq.com>
2023-01-30 23:20:47 +0800

    从前序与中序遍历序列构造二叉树 代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/ConstructBinaryTreeFromPreAndInOrder.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/tree/ConstructBinaryTreeFromPreAndInOrder.java
@@ -2,60 +2,43 @@ package com.mxl2020.algorithms.practise.tree;
 
 import com.mxl2020.algorithms.practise.tree.datastructure.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 从前序与中序遍历序列构造二叉树
  *
  * @see <a href="https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/">LeetCode 105</a>
  */
 public class ConstructBinaryTreeFromPreAndInOrder {
-    // 注意：给定前序和后序，无法重构二叉树
 
     private int[] preorder;
-    private int[] inorder;
+    private Map<Integer, Integer> map;
 
     /**
-     * 分治解法
-     *
      * @param preorder 前序遍历序列，可以确定根节点的位置
      * @param inorder  中序遍历序列，可以确定左右子树的范围
      * @return 返回被还原的树的根节点
-     * <p>
-     * preorder 和 inorder 的长度相同，元素唯一
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        final int n = preorder.length;
         this.preorder = preorder;
-        this.inorder = inorder;
-        return buildTree(0, preorder.length - 1, 0);
-    }
-
-    private TreeNode buildTree(int preLeftIndex, int preRightIndex, int inLeftIndex) {
-        // 递归终止条件
-        if (preLeftIndex > preRightIndex) return null;
-        // 生成根节点
-        int rootValue = this.preorder[preLeftIndex];
-        TreeNode root = new TreeNode(rootValue);
-
-        // 找到根节点在中序遍历序列中的下标
-        // 根据 rootIndexInInOrder 可以定位左右子树的范围
-        int rootIndexInInOrder = inLeftIndex;
-        while (inorder[rootIndexInInOrder] != rootValue) {
-            rootIndexInInOrder++;
+        this.map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
         }
 
-        // 递归构建左子树
-        root.left = buildTree(
-            preLeftIndex + 1,
-            preLeftIndex + rootIndexInInOrder - inLeftIndex,
-            inLeftIndex
-        );
-        // 递归构建右子树
-        root.right = buildTree(
-            preLeftIndex + rootIndexInInOrder - inLeftIndex + 1,
-            preRightIndex,
-            rootIndexInInOrder + 1
-        );
+        return buildTree(0, n - 1, 0, n - 1);
+    }
 
-        // 返回根节点
+    private TreeNode buildTree(int ps, int pe, int is, int ie) {
+        if (ps > pe) return null;
+        TreeNode root = new TreeNode(preorder[ps]);
+        int iRoot = map.get(preorder[ps]);
+
+        root.left = buildTree(ps + 1, ps + (iRoot - is), is, iRoot - 1);
+        root.right = buildTree(ps + (iRoot - is) + 1, pe, iRoot + 1, ie);
+
         return root;
     }
 
