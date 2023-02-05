3cd369a9d2b818ccecf77802c0d69fba2aa21f1d
xiaolongma <3411971052@qq.com>
2023-02-05 17:32:01 +0800

    N 皇后 代码优化

--- algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/dfs/N_Queens.java
+++ algorithms-practise/src/main/java/com/mxl2020/algorithms/practise/search/dfs/N_Queens.java
@@ -21,9 +21,7 @@ public class N_Queens {
     public List<List<String>> solveNQueens(int n) {
         this.n = n;
         this.chessboard = new char[n][n];
-        for (char[] row : chessboard) {
-            Arrays.fill(row, '.');
-        }
+        for (char[] row : chessboard) Arrays.fill(row, '.');
         this.combinations = new ArrayList<>();
         solve(0);
         return combinations;
@@ -36,18 +34,17 @@ public class N_Queens {
     private void solve(int queenNum) {
         if (queenNum == n) {
             List<String> combination = new ArrayList<>(n);
-            for (char[] row : chessboard) {
-                combination.add(new String(row));
-            }
+            for (char[] row : chessboard) combination.add(new String(row));
             combinations.add(combination);
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            if (!availablePosition(queenNum, i)) continue;
-            chessboard[queenNum][i] = 'Q';
-            solve(queenNum + 1);
-            chessboard[queenNum][i] = '.';
+            if (availablePosition(queenNum, i)) {
+                chessboard[queenNum][i] = 'Q';
+                solve(queenNum + 1);
+                chessboard[queenNum][i] = '.';
+            }
         }
     }
 
@@ -55,12 +52,12 @@ public class N_Queens {
         for (int i = 0; i < x; i++) {
             // 判断当前列是否有皇后
             if (chessboard[i][y] == 'Q') return false;
-            // 判断 \ 对角线是否有皇后
-            int leftDiagonalIndex = y - x + i;
-            if (leftDiagonalIndex >= 0 && chessboard[i][leftDiagonalIndex] == 'Q') return false;
-            // 判断 / 对角线是否有皇后
-            int rightDiagonalIndex = y + x - i;
-            if (rightDiagonalIndex < n && chessboard[i][rightDiagonalIndex] == 'Q') return false;
+        }
+
+        for (int i = 1; i <= x; i++) {
+            // 判断同一斜线是否有皇后
+            if ((x - i) >= 0 && (y - i) >= 0 && chessboard[x - i][y - i] == 'Q') return false;
+            if ((x - i) >= 0 && (y + i) < n && chessboard[x - i][y + i] == 'Q') return false;
         }
         return true;
     }
